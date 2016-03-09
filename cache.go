449c5f7612e807572bfc68d564b375f9e8353858
vivek <vivek.gupta@goibibo.com>
2016-03-09 16:31:28 +0530

    adding commenting for godoc

--- cache.go
+++ cache.go
@@ -8,8 +8,12 @@ import (
 	"sync"
 )
 
+// SHARD_COUNT is the number of golang maps that are going to participate in caching
+// internally.
 var SHARD_COUNT int
 
+// lowSpaceError is the error that is thrown when the cache object is capable of
+// caching the given value ,but the cache object is running out of memory.
 type lowSpaceError struct {
 	errorNumber int
 	problem string
@@ -19,6 +23,8 @@ func (lse *lowSpaceError) Error() string {
 	return fmt.Sprintf("%d---%s", lse.errorNumber, lse.problem)
 }
 
+// sizeLimitError is the error which is thrown when the element size is larger
+// than the total memory allocated to the cache object.
 type sizeLimitError struct {
 	errorNumber int
 	problem string
@@ -28,26 +34,16 @@ func (sle *sizeLimitError) Error() string {
 	return fmt.Sprintf("%d---%s", sle.errorNumber, sle.problem)
 }
 var (
+	// SizeLimitError returns when size of value is greater then total allocated memory
 	SizeLimitError = &sizeLimitError{problem:"data size is more than max size", errorNumber: 0}
+	// LowSpaceError returns when size of the value is greater than current available space
 	LowSpaceError = &lowSpaceError{problem:"space not available", errorNumber: 1}
 )
 
-// A list of maps to support cache partition for threadsafe behaviour
-type CacheData struct {
-	 MapList []*ThreadSafeMap
-}
-
-func (c *CacheData) getShardMap(key string) *ThreadSafeMap {
-	hasher := fnv.New32()
-	hasher.Write([]byte(key))
-	return c.MapList[uint(hasher.Sum32())%uint(SHARD_COUNT)]
-}
-
-// A thread safe string to interface{} map.
+// ThreadSafeMap is a thread string to interface{} map.
 type ThreadSafeMap struct {
 	Items        map[string]interface{}
 	sync.RWMutex // Read Write mutex, guards access to internal map.
-	//sync.Mutex
 }
 
 func (tsm *ThreadSafeMap) String() string{
@@ -56,8 +52,31 @@ func (tsm *ThreadSafeMap) String() string{
 	return fmt.Sprintf("{currentsize:%v, data:%v}", len(tsm.Items), tsm.Items)
 }
 
+// CacheData is list of threadsafe maps to participate in cache partition.
+type CacheData struct {
+	 MapList []*ThreadSafeMap
+}
+
+// getShardMap returns the internal map which is supposed to keep the value
+// for this key. This method internally uses hashing on the key and finds out
+// the internal cache map.
+func (c *CacheData) getShardMap(key string) *ThreadSafeMap {
+	hasher := fnv.New32()
+	hasher.Write([]byte(key))
+	return c.MapList[uint(hasher.Sum32())%uint(SHARD_COUNT)]
+}
+
+// Cache is the stucture resposible to handle the cache key and value.
+// This structure having the following parameters in it :
+//			MaxSize: maximum size of the cache object
+//			CurrentSize: current size of the cache object
+//			Size: a golang map to store the values size key wise.
+//				  this structure save the cpu cyclone to calculate he size
+// 				  many time
+//			Data: a pointer to the CacheData in which threadsafe maps are
+// 				  storing the values
+//			sync.RWMutex: to ensure the thread safety for this structure
 type Cache struct {
-	//CacheMode bool to be added later
 	MaxSize int
 	CurrentSize int
 	Size map[string]int
@@ -65,6 +84,7 @@ type Cache struct {
 	sync.RWMutex // for atomic CurrentSize modification
 }
 
+// GetCurrentSize return the current size of the cache.
 func (c *Cache) GetCurrentSize() int {
 	c.RLocker().Lock()
 	defer c.RLocker().Unlock()
@@ -75,22 +95,46 @@ func (c *Cache) GetCurrentSize() int {
 	return totalSize
 }
 
-
 func (c *Cache)String() string {
 	c.RLocker().Lock()
 	defer c.RLocker().Unlock()
 	return fmt.Sprintf("{currentsize:%v, data:%v}", c.CurrentSize, c.Data)
 }
 
-
-func (c *Cache)CacheIterator(outputChannel chan map[string]interface{} ) {
-	c.RLocker().Lock()
-	defer c.RLocker().Unlock()
-	for i := 0; i < SHARD_COUNT; i++ {
-		 outputChannel <- c.Data.MapList[i].Items
-	}
+// CacheRow is a entry in cache having structure like (key: value)
+type CacheRow struct{
+	Key string
+	Value interface{}
 }
 
+func (c CacheRow)String() string {
+	fmt.Printf("name is vivek\n")
+	return fmt.Sprintf("cache row {key:%v, value:%v}", c.Key, c.Value)
+}
+
+// CacheIterator returns all the key and value pair  one by one in the
+// input CacheRow channel. Calling goroutine  returns immediately after
+// spawning a gourtine. Spawned goroutine fills the channel with CacheRow.
+// When there is not any CacheRow left, spawned goroutine closes the channel.
+func (c *Cache)CacheIterator(outputChannel chan CacheRow) {
+	go func(){
+		c.RLocker().Lock()
+		defer c.RLocker().Unlock()
+		for i := 0; i < SHARD_COUNT; i++ {
+			for key, value := range c.Data.MapList[i].Items{
+				temp := CacheRow{key, value}
+				outputChannel<- temp
+			}
+		}
+		close(outputChannel)
+	}()
+}
+
+// CacheGet returns the value in the cache pointed by the
+// input key parameter.
+// return values :
+//		ok: true if success else false
+//		val: value corresponding to the key
 func (c *Cache) CacheGet(key string)(interface{}, bool){
 	c.RLocker().Lock()
 	defer c.RLocker().Unlock()
@@ -101,6 +145,12 @@ func (c *Cache) CacheGet(key string)(interface{}, bool){
 	return val, ok
 }
 
+// CacheSet sets the key with its corresponding value in the cache.
+// In case of memory unavailability, It frees some space with the defined algorithm
+// and sets the key.size of the key is in bytes.
+// return values :
+//		success: true if success else false
+//		error: error in the operation
 func (c *Cache) CacheSet(key string, value interface{}, size int) (bool, error){
 	success, error := c.SetData(key, value, size)
 	for error == LowSpaceError{
@@ -110,6 +160,11 @@ func (c *Cache) CacheSet(key string, value interface{}, size int) (bool, error){
 	return success, error
 }
 
+// makeSpace frees the memory to accommodate new key as given in the input
+// params with its size.
+// return values :
+//		success: true if success else false
+//		error: error in the operation
 func (c *Cache)makeSpace(key string, size int) (bool, error){
 	sharedMap := c.Data.getShardMap(key)
 	sharedMap.RLocker().Lock()
@@ -125,6 +180,7 @@ func (c *Cache)makeSpace(key string, size int) (bool, error){
 	return true, nil
 }
 
+// deleteRandomKey deletes a random key present in the cache.
 func (c *Cache) deleteRandomKey()  {
 	c.Lock()
 	defer c.Unlock()
@@ -143,6 +199,10 @@ func (c *Cache) deleteRandomKey()  {
 	delete(c.Size, deletedkey)
 }
 
+// isSpaceAvaible returns an boolean identifier that tells, if
+// space is available for the given key.
+// returns :
+//		retFlag: true if space is available else false
 func (c *Cache) isSpaceAvaible(key string, size int) (bool) {
 	sharedMap := c.Data.getShardMap(key)
 	sharedMap.RLocker().Lock()
@@ -181,6 +241,12 @@ func (c *Cache) isNewValueLarger(key string, size int) (bool) {
 	return retFlag
 }
 
+// SetData sets a key and its corresponding value in the cache.
+// This methods is responsible to set in the cache when there is other
+// algorithm is applied for key eviction in case of memory unavailability.
+// returns :
+//		retFlag: true if space is available else false
+//		error: if any error in the operation else nil
 func (c *Cache) SetData(key string, value interface{}, size int) (bool, error){
 	// locking currentSize atomic lock
 	c.Lock()
@@ -199,6 +265,7 @@ func (c *Cache) SetData(key string, value interface{}, size int) (bool, error){
 	return true, nil
 }
 
+// CacheDelete deletes the key in the cache.
 func (c *Cache)CacheDelete(key string)  {
 	c.Lock()
 	defer c.Unlock()
@@ -210,16 +277,19 @@ func (c *Cache)CacheDelete(key string)  {
 	delete(c.Size, key)
 }
 
+// GetDefaultCache returns the most abstract cache just using the
+// cap in memory limit . Cache is having algorithm to evict key when
+// space is not available in random selection.
 func GetDefaultCache(cacheSize int, cachePartitions int) *Cache {
+	SHARD_COUNT = cachePartitions
 	newCache := &Cache{
-		Data: &CacheData{MapList:make([]*ThreadSafeMap, cachePartitions)},
+		Data: &CacheData{MapList:make([]*ThreadSafeMap, SHARD_COUNT)},
 		Size: make(map[string]int),
 	}
 
-	for i := 0; i < cachePartitions; i++ {
+	for i := 0; i < SHARD_COUNT; i++ {
 		newCache.Data.MapList[i] = &ThreadSafeMap{Items: make(map[string]interface{})}
 	}
 	newCache.MaxSize = cacheSize
-	SHARD_COUNT = cachePartitions
 	return newCache
 }
--- volatileLRUCache.go
+++ volatileLRUCache.go
@@ -21,7 +21,6 @@ import (
 //	    root prev \      /root next
 //				    ROOT
 //
-//
 type Link struct {
 	key string
 	ExpireTime time.Time
@@ -32,13 +31,17 @@ type Link struct {
 	lruNext *Link
 }
 
+// isLinkTTLExpired tells in boolean about the key expiration.
+// true if expired or false.
 func (l *Link)isLinkTTLExpired()(bool)  {
-	fmt.Printf("current time= %v \n", time.Now())
-	fmt.Printf("local time = %v \n", l.ExpireTime)
-	fmt.Printf("local expired = %v \n", l.ExpireTime.Before(time.Now()))
+	//fmt.Printf("current time= %v \n", time.Now())
+	//fmt.Printf("local time = %v \n", l.ExpireTime)
+	//fmt.Printf("local expired = %v \n", l.ExpireTime.Before(time.Now()))
 	return l.ExpireTime.Before(time.Now())
 }
 
+// addLRULink adds a lru link in the circular doubly link list between root
+// and a node left of it.
 func (l *Link)addLRULink(temp *Link )  {
 	l.lruNext = temp
 	l.lruPrev = temp.lruPrev
@@ -46,6 +49,8 @@ func (l *Link)addLRULink(temp *Link )  {
 	temp.lruPrev = l
 }
 
+// addLRULink adds a ttl link in the circular doubly link list between root
+// and a node left of it.
 func (l *Link)addTTLLink(temp *Link )  {
 	l.ttlNext = temp
 	l.ttlPrev = temp.ttlPrev
@@ -53,6 +58,8 @@ func (l *Link)addTTLLink(temp *Link )  {
 	temp.ttlPrev = l
 }
 
+// unlinkLRULink unlinks the link from its lru pointers in the
+// doubly link list
 func (temp *Link) unlinkLRULink()  {
 	nextLink := temp.lruNext
 	prevLink := temp.lruPrev
@@ -60,6 +67,8 @@ func (temp *Link) unlinkLRULink()  {
 	prevLink.lruNext = nextLink
 }
 
+// unlinkTTLLink unlinks the link from its ttl pointers in the
+// doubly link list
 func (temp *Link) unlinkTTLLink()  {
 	nextLink := temp.ttlNext
 	prevLink := temp.ttlPrev
@@ -67,16 +76,28 @@ func (temp *Link) unlinkTTLLink()  {
 	prevLink.ttlNext = nextLink
 }
 
+// unlink removes a link in circular doubly link list.
 func (temp *Link) unlink()  {
 	temp.unlinkLRULink()
 	temp.unlinkTTLLink()
 }
-
+// add insert a new link in circular doubly link list
 func (l *Link) add(temp *Link)  {
 	l.addLRULink(temp)
 	l.addTTLLink(temp)
 }
 
+// VolatileLRUCache is a cache wrapper on top of Cache.
+// so still the maximum size of the cache is controlled
+// by Cache only. This wrapper just adds an algorithm for
+// key eviction policy .
+// In case of memory unavailability VolatileLRUCache deletes
+// the keys in the following order :
+// 		*** keys which has been expired then the keys which are
+//		*** keys which are least recently used
+// VolatileLRUCache maintains a circular doubly link list in memory
+// to have the meta data of the keys ready. Also this structure is
+// thread safe; meaning several goroutine can operate concurrently.
 type VolatileLRUCache struct {
 	cache *Cache
 	root *Link
@@ -85,6 +106,8 @@ type VolatileLRUCache struct {
 	sync.RWMutex // to make double linked list thread safe
 }
 
+// GetCurrentSize is a wrapper on top of Cache GetCurrentSize
+// which returns the current VolatileLRUCache size in bytes.
 func (vlruCache *VolatileLRUCache) GetCurrentSize() int {
 	vlruCache.RLocker().Lock()
 	defer vlruCache.RLocker().Unlock()
@@ -101,6 +124,7 @@ func  (vlruCache *VolatileLRUCache) String() string  {
 	return buffer.String()
 }
 
+// GetLRUInfo return the lru information of the keys in VolatileLRUCache.
 func (vlruCache *VolatileLRUCache) GetLRUInfo() string {
 	vlruCache.RLocker().Lock()
 	defer vlruCache.RLocker().Unlock()
@@ -120,6 +144,7 @@ func (vlruCache *VolatileLRUCache) GetLRUInfo() string {
 	return buffer.String()
 }
 
+// GetLRUInfo return the ttl information of the keys in VolatileLRUCache.
 func (vlruCache *VolatileLRUCache) GetTTLInfo() string {
 	vlruCache.RLocker().Lock()
 	defer vlruCache.RLocker().Unlock()
@@ -139,28 +164,43 @@ func (vlruCache *VolatileLRUCache) GetTTLInfo() string {
 	return buffer.String()
 }
 
-func (vlruCache *VolatileLRUCache)VolatileLRUCacheIterator(outputChannel chan interface{} ) {
-	vlruCache.RLocker().Lock()
-	defer vlruCache.RLocker().Unlock()
-	rootLink := vlruCache.root
-	startingLink := rootLink.ttlNext
-	for ;startingLink != rootLink; {
-		if !startingLink.isLinkTTLExpired(){
-			val, _ := vlruCache.cache.CacheGet(startingLink.key)
-			outputChannel <- val
+func (vlruCache *VolatileLRUCache)VolatileLRUCacheIterator(outputChannel chan CacheRow) {
+	go func() {
+		vlruCache.RLocker().Lock()
+		defer vlruCache.RLocker().Unlock()
+		rootLink := vlruCache.root
+		// taking ttl pointer to start with the oldest key
+		startingLink := rootLink.ttlNext
+		for ;startingLink != rootLink; {
+			if !startingLink.isLinkTTLExpired(){
+				val, ok := vlruCache.cache.CacheGet(startingLink.key)
+				if ok{
+					outputChannel <- CacheRow{Key:startingLink.key, Value:val}
+				}
+			}
+			nextLink := startingLink.ttlNext
+			startingLink = nextLink
 		}
-		nextLink := startingLink.ttlNext
-		startingLink = nextLink
-	}
+		close(outputChannel)
+		fmt.Printf("spawaned go routine finishes")
+	}()
 }
 
+// VolatileLRUCacheGet returns the value corresponding to a key present in Cache.
+// This also modify internal doubly link list to maintain the updated ttl and lru info
+// of the keys preset in Cache.
+// return values :
+//		value: value corresponding to the key
+//		ok: true if success else false
 func (vlruCache *VolatileLRUCache) VolatileLRUCacheGet(key string)(interface{}, bool){
+	// lower level is thread safe so making write lock after this.
 	value, ok := vlruCache.cache.CacheGet(key)
-	fmt.Printf("cache value = %v error is = %v" , value, ok)
-	// changing the link so grabing write lock
+	//fmt.Printf("cache value = %v error is = %v" , value, ok)
+	// changing the link so grabbing write lock
 	vlruCache.Lock()
 	defer vlruCache.Unlock()
 	keyLink := vlruCache.linkMap[key]
+	//fmt.Printf("exsting link was %v", keyLink)
 	if !ok || keyLink.isLinkTTLExpired(){
 		return nil, false
 	}
@@ -169,6 +209,20 @@ func (vlruCache *VolatileLRUCache) VolatileLRUCacheGet(key string)(interface{}, 
 	return value, ok
 }
 
+// VolatileLRUCacheSet sets the value corresponding to a key in Cache.
+// Setting operation also removes the keys which are already expired ; so as to
+// make the rem free as much as possible. In case of memory is not available
+// even after removing expired keys it removes the lru keys.
+// This also modify internal doubly link list to maintain the updated ttl and lru info
+// of the keys preset in Cache.
+//
+// input params :
+//				key: key to hold the value in cache (string type)
+//				value: struct having the data to cache.
+//				keyExpire: time duration for the current key expire.
+// return values :
+//		ok: true if operation is successful else false
+//		error: error in case of occurred error else nil
 func (vlruCache *VolatileLRUCache) VolatileLRUCacheSet(key string, value interface{}, size int, keyExpire time.Duration) (bool, error){
 	//free memory from expired keys
 	vlruCache.Lock()
@@ -198,6 +252,7 @@ func (vlruCache *VolatileLRUCache) VolatileLRUCacheSet(key string, value interfa
 	return true, nil
 }
 
+// RemoveVolatileKey removes the keys which are already expired in VolatileLRUCache.
 func(vlruCache *VolatileLRUCache) RemoveVolatileKey() {
 	rootLink := vlruCache.root
 	startingLink := rootLink.ttlNext
@@ -212,22 +267,32 @@ func(vlruCache *VolatileLRUCache) RemoveVolatileKey() {
 	}
 }
 
+// VolatileLRUCacheDelete deletes a key present in VolatileLRUCache.
 func (vlruCache *VolatileLRUCache)VolatileLRUCacheDelete(key string)  {
-	vlruCache.Lock()
-	defer vlruCache.Unlock()
-	vlruCache.RemoveVolatileKey()
-	vlruCache.cache.CacheDelete(key)
-	deletedLink := vlruCache.linkMap[key]
-	deletedLink.unlink()
-	delete(vlruCache.linkMap, key)
+	// lower level is thread safe so making write lock after this.
+	_, ok := vlruCache.cache.CacheGet(key)
+	if ok{
+		// changing the link so grabbing write lock
+		vlruCache.Lock()
+		defer vlruCache.Unlock()
+		vlruCache.RemoveVolatileKey()
+		vlruCache.cache.CacheDelete(key)
+		deletedLink := vlruCache.linkMap[key]
+		deletedLink.unlink()
+		delete(vlruCache.linkMap, key)
+	}
 }
 
+// makeSpace frees the space with least recently key.
+// return values :
+//		ok: true if operation is successful else false
+//		error: error in case of occurred error else nil
 func (vlruCache *VolatileLRUCache)makeSpace() (bool, error){
 	fmt.Printf("clearing space in volatile cache\n")
 	// linkTBE means link to be evicted with its data(key, value) in cache
 	linkTBE := vlruCache.root.lruNext
 	if linkTBE == vlruCache.root{
-		return false, errors.New("volatileLRUcache is empty ... May be the memory is less")
+		return false, errors.New("VolatileLRUCache is empty ... May be the memory is less")
 	}
 	key := linkTBE.key
 	vlruCache.cache.CacheDelete(key)
@@ -236,6 +301,11 @@ func (vlruCache *VolatileLRUCache)makeSpace() (bool, error){
 	return true, nil
 }
 
+// GetVolatileLRUCache returns an instance of VolatileLRUCache with the specified
+// input params:
+//			cacheSize: size of the cache in bytes
+//			cachePartitions: total number map participating in internal cache.
+//			ttl: a global time duration for each key expiration.
 func GetVolatileLRUCache(cacheSize int, cachePartitions int, ttl time.Duration) *VolatileLRUCache {
 	newVolatileCache := &VolatileLRUCache{
 		cache: GetDefaultCache(cacheSize, cachePartitions),
@@ -245,8 +315,8 @@ func GetVolatileLRUCache(cacheSize int, cachePartitions int, ttl time.Duration) 
 	//converting ttl to seconds for microseconds
 	ttl = ttl * time.Second
 	newVolatileCache.globalTTL = ttl
-	fmt.Printf("global ttl  .... %v \n", newVolatileCache.globalTTL)
-	fmt.Printf("current time.... %v \n", time.Now())
+	//fmt.Printf("global ttl  .... %v \n", newVolatileCache.globalTTL)
+	//fmt.Printf("current time.... %v \n", time.Now())
 	newVolatileCache.root.lruNext = newVolatileCache.root
 	newVolatileCache.root.lruPrev = newVolatileCache.root
 	newVolatileCache.root.ttlNext = newVolatileCache.root
